f8c13fc24a07f8fbe4941c7cdfd335bab8e5f57f
red-drop <junsu.jang.tech@gmail.com>
2020-10-10 23:31:58 +0900

    trasaction test

--- src/main/java/jpaTutorial/Member.java
+++ src/main/java/jpaTutorial/Member.java
@@ -5,8 +5,6 @@ import javax.persistence.Id;
 
 @Entity
 public class Member {
-
-
     @Id
     private Long id;
     private String name;
--- src/main/java/jpaTutorial/jpaMain.java
+++ src/main/java/jpaTutorial/jpaMain.java
@@ -2,6 +2,7 @@ package jpaTutorial;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class jpaMain {
@@ -9,11 +10,27 @@ public class jpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("study_jpa");
         EntityManager em = emf.createEntityManager();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("User1");
-        em.persist(member);
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(5L);
+            member.setName("gang");
+            em.persist(member);
+            tx.commit();
+
+            Member m = em.find(Member.class, 1L);
+            System.out.println("MemberId: " + m.getId());
+            System.out.println("MemberName: " + m.getName());
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+
         emf.close();
     }
 }
